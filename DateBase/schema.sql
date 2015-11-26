04b5162d567af5dfdacef3e07a3965d9f5d9657e
Sarkel <sebastian.kubalski@gmail.com>
2015-11-26 14:55:28 +0100

    Datebase schemat update

--- DateBase/schema.sql
+++ DateBase/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE "Ksiazka" (
 	"Avatar" serial NOT NULL,
 	"Wydawnictwo" serial NOT NULL,
 	"Liczba_egzemplarzy" int NOT NULL DEFAULT '0',
-	"Wypozyczone" int NOT NULL DEFAULT '0',
+	"Wypozyczona" BOOLEAN NOT NULL DEFAULT 'false',
 	CONSTRAINT Ksiazka_pk PRIMARY KEY ("Ksiazka_ID")
 ) WITH (
   OIDS=FALSE
@@ -57,12 +57,12 @@ CREATE TABLE "Autor" (
 
 
 
-CREATE TABLE "Nalezkosci" (
-	"Nalezkosci_ID" serial NOT NULL,
+CREATE TABLE "Nalezkosc" (
+	"Nalezkosc_ID" serial NOT NULL,
 	"Uzytkownik" serial NOT NULL,
 	"Opis" TEXT(80) NOT NULL,
 	"Wartosc" numeric(10,2) NOT NULL,
-	CONSTRAINT Nalezkosci_pk PRIMARY KEY ("Nalezkosci_ID")
+	CONSTRAINT Nalezkosc_pk PRIMARY KEY ("Nalezkosc_ID")
 ) WITH (
   OIDS=FALSE
 );
@@ -70,11 +70,12 @@ CREATE TABLE "Nalezkosci" (
 
 
 CREATE TABLE "Wypozyczona_ksiazka" (
+	"Wypozyczona_ksiazka_ID" serial NOT NULL,
 	"Uzytkownik" serial NOT NULL,
 	"Ksiazka" serial NOT NULL,
 	"Data_wypozyczenia" DATE NOT NULL,
 	"Data_oddania" DATE NOT NULL,
-	CONSTRAINT Wypozyczona_ksiazka_pk PRIMARY KEY ("Uzytkownik","Ksiazka")
+	CONSTRAINT Wypozyczona_ksiazka_pk PRIMARY KEY ("Wypozyczona_ksiazka_ID","Uzytkownik","Ksiazka")
 ) WITH (
   OIDS=FALSE
 );
@@ -161,23 +162,35 @@ CREATE TABLE "Komentarz" (
 
 
 
-CREATE TABLE "Wiadomość" (
-	"Wiadomość_ID" serial NOT NULL,
+CREATE TABLE "Wiadomosc" (
+	"Wiadomosc_ID" serial NOT NULL,
 	"Adresat" serial NOT NULL,
 	"Odbiorca" serial NOT NULL,
 	"Tekst" TEXT NOT NULL,
 	"Data" DATE NOT NULL,
-	CONSTRAINT Wiadomość_pk PRIMARY KEY ("Wiadomość_ID")
+	CONSTRAINT Wiadomosc_pk PRIMARY KEY ("Wiadomosc_ID")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "Rodzaj_użytkownika" (
-	"Rodzaj_użytkownika_ID" serial NOT NULL,
+CREATE TABLE "Rodzaj_uzytkownika" (
+	"Rodzaj_uzytkownika_ID" serial NOT NULL,
 	"Nazwa" serial NOT NULL,
-	CONSTRAINT Rodzaj_użytkownika_pk PRIMARY KEY ("Rodzaj_użytkownika_ID")
+	CONSTRAINT Rodzaj_uzytkownika_pk PRIMARY KEY ("Rodzaj_uzytkownika_ID")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "Ocena" (
+	"Ocena_ID" serial NOT NULL,
+	"Ksiazka" serial NOT NULL,
+	"Ilosc_gwizdek" int NOT NULL,
+	"Komentarz" TEXT,
+	CONSTRAINT Ocena_pk PRIMARY KEY ("Ocena_ID")
 ) WITH (
   OIDS=FALSE
 );
@@ -187,14 +200,14 @@ CREATE TABLE "Rodzaj_użytkownika" (
 ALTER TABLE "Uzytkownik" ADD CONSTRAINT "Uzytkownik_fk0" FOREIGN KEY (Avatar) REFERENCES Avatar(Avatar_ID);
 ALTER TABLE "Uzytkownik" ADD CONSTRAINT "Uzytkownik_fk1" FOREIGN KEY (Adres) REFERENCES Adres(Adres_ID);
 ALTER TABLE "Uzytkownik" ADD CONSTRAINT "Uzytkownik_fk2" FOREIGN KEY (Kontakt) REFERENCES Kontakt(Kontakt_ID);
-ALTER TABLE "Uzytkownik" ADD CONSTRAINT "Uzytkownik_fk3" FOREIGN KEY (Typ) REFERENCES Rodzaj_użytkownika(Rodzaj_użytkownika_ID);
+ALTER TABLE "Uzytkownik" ADD CONSTRAINT "Uzytkownik_fk3" FOREIGN KEY (Typ) REFERENCES Rodzaj_uzytkownika(Rodzaj_uzytkownika_ID);
 
 ALTER TABLE "Ksiazka" ADD CONSTRAINT "Ksiazka_fk0" FOREIGN KEY (Avatar) REFERENCES Avatar(Avatar_ID);
 ALTER TABLE "Ksiazka" ADD CONSTRAINT "Ksiazka_fk1" FOREIGN KEY (Wydawnictwo) REFERENCES Wydawnictwo(Wydawnictwo_ID);
 
 
 
-ALTER TABLE "Nalezkosci" ADD CONSTRAINT "Nalezkosci_fk0" FOREIGN KEY (Uzytkownik) REFERENCES Uzytkownik(Uzytkownik_ID);
+ALTER TABLE "Nalezkosc" ADD CONSTRAINT "Nalezkosc_fk0" FOREIGN KEY (Uzytkownik) REFERENCES Uzytkownik(Uzytkownik_ID);
 
 ALTER TABLE "Wypozyczona_ksiazka" ADD CONSTRAINT "Wypozyczona_ksiazka_fk0" FOREIGN KEY (Uzytkownik) REFERENCES Uzytkownik(Uzytkownik_ID);
 ALTER TABLE "Wypozyczona_ksiazka" ADD CONSTRAINT "Wypozyczona_ksiazka_fk1" FOREIGN KEY (Ksiazka) REFERENCES Ksiazka(Ksiazka_ID);
@@ -212,7 +225,9 @@ ALTER TABLE "Kontakt" ADD CONSTRAINT "Kontakt_fk0" FOREIGN KEY (Poczta) REFERENC
 ALTER TABLE "Komentarz" ADD CONSTRAINT "Komentarz_fk0" FOREIGN KEY (Urzytkownik) REFERENCES Uzytkownik(Uzytkownik_ID);
 ALTER TABLE "Komentarz" ADD CONSTRAINT "Komentarz_fk1" FOREIGN KEY (Ksiazka) REFERENCES Ksiazka(Ksiazka_ID);
 
-ALTER TABLE "Wiadomość" ADD CONSTRAINT "Wiadomość_fk0" FOREIGN KEY (Adresat) REFERENCES Uzytkownik(Uzytkownik_ID);
-ALTER TABLE "Wiadomość" ADD CONSTRAINT "Wiadomość_fk1" FOREIGN KEY (Odbiorca) REFERENCES Uzytkownik(Uzytkownik_ID);
+ALTER TABLE "Wiadomosc" ADD CONSTRAINT "Wiadomosc_fk0" FOREIGN KEY (Adresat) REFERENCES Uzytkownik(Uzytkownik_ID);
+ALTER TABLE "Wiadomosc" ADD CONSTRAINT "Wiadomosc_fk1" FOREIGN KEY (Odbiorca) REFERENCES Uzytkownik(Uzytkownik_ID);
 
+
+ALTER TABLE "Ocena" ADD CONSTRAINT "Ocena_fk0" FOREIGN KEY (Ksiazka) REFERENCES Ksiazka(Ksiazka_ID);
 
